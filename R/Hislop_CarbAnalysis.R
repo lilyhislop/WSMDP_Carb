78ec03e78644febaf869b5492d90ce4923533e6a
Lily Hislop <lilyhislop@gmail.com>
2021-03-01 15:31:06 -0600

    SampleCleanup
    Changed around file name too

--- R/Hislop_CarbAnalysis.R
+++ R/Hislop_CarbAnalysis.R
@@ -16,6 +16,8 @@ library(lme4)
 library(ggplot2)
 library(ggpmisc)
 library(grDevices)
+library(tidyr)
+library(stringr)
 
 getwd()
 setwd("C:/Users/LHislop/Documents/GitHub/WSMDP_Carb")
@@ -30,3 +32,13 @@ PredictionStarchFile <-paste("Data/RawData/wsmdp2021gl","st","plsall.txt", sep =
 PredictionSugarFile <-paste("Data/RawData/wsmdp2021gl","su","plsall.txt", sep = "")
 
 CarbPredData <- data.frame(AgPredOutput(PredictionStarchFile,PredictionSugarFile))
+
+CarbPredData <- separate(CarbPredData, Samples, into = c("Year","Row","Rep"), sep = "([W.Y.-])", remove = FALSE)
+CarbPredData$Location <- "Wi"
+CarbPredData$Location[which(grepl("N",CarbPredData$Year))] = "NY"
+CarbPredData$Year<-str_remove(CarbPredData$Year,"N")
+CarbPredData <- CarbPredData[-c(1:2),]
+tail(CarbPredData)
+
+
+summary(CarbPredData)
